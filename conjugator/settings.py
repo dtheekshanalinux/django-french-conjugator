6bfc200cced4c836bf91124f3059dff84d1028f7
dtheekshanalinux <dinindu.linux@gmail.com>
2021-09-20 02:04:22 +0530

    Add statics and templates

--- conjugator/settings.py
+++ conjugator/settings.py
@@ -45,7 +45,7 @@ ROOT_URLCONF = 'conjugator.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['TEMPLATES_DIR'],
+        'DIRS': [TEMPLATES_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -66,8 +66,12 @@ WSGI_APPLICATION = 'conjugator.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': config('NAME'),
+        'USER': config('USER'),
+        'PASSWORD': config('PASSWORD'),
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
--- french/views.py
+++ french/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+import requests
+from requests.compat import quote_plus # this thing add  sign to spaces
+from bs4 import BeautifulSoup
+from . import models
 
-# Create your views here.
+def home(requests):
+    return render(requests, 'base.html')
+
+def new_search(requests):
+    return render(requests, 'index.html')
