9705c4c411111b1eadb86f841cd7c0aaab447c99
dtheekshanalinux <dinindu.linux@gmail.com>
2021-09-24 22:52:26 +0530

    Add posts for cards

--- french/views.py
+++ french/views.py
@@ -23,8 +23,6 @@ def new_search(request):
     #all content we want
     card_postings = []
     
-    #post_title_listings = post_title_detail()
-
     # body card detail 
     card_detail = soup.find_all('div',{'class':'word-wrap-title'})
     for card in card_detail:
@@ -32,15 +30,33 @@ def new_search(request):
 
         card_postings.append((card_title))
     print(card_postings)
+    
+    num_list =[]
+    
+    post_detail = soup.find_all('div',{'class':'blue-box-wrap'})
+    for div in post_detail:
+        post = div.find('p').text
+        num_list.append((post))
+    
+    arrays = [[num_list[0]]] # array of sub-arrays (starts with first value)
 
+    for i in range(1, len(num_list)): # go through each element after the first
+        if num_list[i] != "Présent": # If it's larger than the previous
+            arrays[len(arrays)-1].append(num_list[i]) # Add it to the last sub-array
+        else: # otherwise
+            arrays.append([num_list[i]])
+    #print(arrays)
     # post title
-     
+    list1 = card_postings
+    list2 = arrays
+    zipped_lists = zip(card_postings, arrays) 
     # this function is for get that how many cards for specific title 
 
 
     stuff_for_frontend = {
     'search':search,
     'card_postings':card_postings,
+    'zipped_lists':zipped_lists,
     }
     return render(request, 'index.html', stuff_for_frontend)
     
